28691a4eece07d9300ad8ee0b26cc411015cebf0
Fredrik Björeman <fredrik.bjoreman@timeedit.com>
2018-10-17 11:38:24 +0200

    Opened in Xcode 10, built, updated settings.

--- Source/UKBorderlessWindow.m
+++ Source/UKBorderlessWindow.m
@@ -12,7 +12,7 @@
 @implementation UKBorderlessWindow
 
 // Designated Initializer:
--(id)   initWithContentRect: (NSRect)box styleMask: (unsigned long)sty backing: (NSBackingStoreType)bs defer: (BOOL)def
+-(id)   initWithContentRect: (NSRect)box styleMask: (NSWindowStyleMask)sty backing: (NSBackingStoreType)bs defer: (BOOL)def
 {
 	self = [super initWithContentRect:box styleMask: NSBorderlessWindowMask backing:bs defer: def];
 	
